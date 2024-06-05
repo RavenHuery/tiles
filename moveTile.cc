b9a17d4fcceba8e7cee767b38e15b4a047168f71
Raven Huery <ravenhuery0@gmail.com>
2024-06-05 15:00:13 -0600

    Added move tile files.

--- moveTile.cc
+++ moveTile.cc
@@ -1,1 +1,22 @@
 //Written by Raven Huery
+
+#include <iomanip>
+#include <vector>
+#include "board.h"
+#include "tileType.h"
+#include "moveTile.h"
+
+using namespace std;
+
+moveTile::shiftTile(tileType a, tileType b) {
+
+}
+
+moveTile::isLegal(tileType a, tileType b) {
+    int aRef = a.getRef(); //primary
+    int bRef = b.getRef(); //secondary
+    bool spaceAvailable; //Is one empty and another numerical?
+    bool simpleShift; //Are the two tiles right next to each other?
+
+    
+}
--- moveTile.h
+++ moveTile.h
@@ -1,3 +1,25 @@
 //Written by Raven Huery
 
 #include <iomanip>
+#include <vector>
+#include "board.h"
+#include "tileType.h"
+
+using namespace std;
+
+class moveTile {
+    public:
+
+    //class constructor
+    moveTile();
+
+    //class destructor
+    ~moveTile();
+
+    //shift a tile from one position to another
+    void shiftTile(tileType, tileType);
+
+    protected:
+    //takes two tiles and returns true if the move is legal and false otherwise
+    bool isLegal(tileType, tileType);
+};
