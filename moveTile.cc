cc81c9c6b041feaae49017f1589104ea651ce905
Raven Huery <ravenhuery0@gmail.com>
2024-06-06 08:11:47 -0600

    moveTile files complete

--- moveTile.cc
+++ moveTile.cc
@@ -8,23 +8,42 @@
 
 using namespace std;
 
-moveTile::shiftTile(tileType a, tileType b) {
-
+void moveTile::shiftTile(tileType a, tileType b) {
+    if (isLegal(a, b) == true) {
+        int newB = a.getRef();
+        int newA = b.getRef();
+        a.setRef(newA);
+        b.setRef(newB);
+        cout << "Shift has been completed!" << endl;
+    }
+    else {
+        cout << "You can't do that! That move is ILLEGAL! Try again... " << endl;
+    }
 }
 
-moveTile::isLegal(tileType a, tileType b) {
+bool moveTile::isLegal(tileType a, tileType b) {
     int aRef = a.getRef(); //primary
     int bRef = b.getRef(); //secondary
-    bool spaceAvailable; //Is one empty and another numerical?
-    bool simpleShift; //Are the two tiles right next to each other?
-
-    
+    bool spaceFree = spaceFree(a, b); //Is one empty and another numerical?
+    bool isAdj = isAdjacent(a, b); //Are the two tiles right next to each other?
+    if (spaceFree == true && isAdj == true) 
+        return true;
+    return false;
 }
 
-moveTile::spaceFree(tileType a, tileType b) {
-
+bool moveTile::spaceFree(tileType a, tileType b) {
+    bool aFree = a.isEmpty();
+    bool bFree = b.isEmpty();
+    if ((aFree == false && bFree == true) || (aFree == true && bFree == false))
+        return true;
+    return false;
 }
 
-moveTile::isAdjacent(tileType a, tileType b) {
-
+bool moveTile::isAdjacent(tileType a, tileType b) {
+    int aRef = a.getRef();
+    int bRef = b.getRef();
+    int diff = aRef - bRef;
+    if ((diff == 1) || (diff == -1) || (diff == 3) || (diff == -3))
+        return true;
+    return false;
 }
